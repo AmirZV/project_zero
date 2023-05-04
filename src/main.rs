b4f75515993002191e4b42d85a92eb8dc104cc1d
Amir Zand <am.zand@outlook.com>
2023-05-05 01:15:24 +0430

    shadowing

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,22 @@ use std::cmp::Ordering;
 //     println!("Hello World!");
 // }
 
-fn main () {
-    println!("Enter your name: ");
-    let mut name = String::new();
-    let greeting = "Nice to meet you";
-    io::stdin().read_line(&mut name)
-        .expect("Dind't Receive Input!");
-    println!("Hello {}!, {}", name.trim_end(), greeting);
+// fn main () {
+//     println!("Enter your name: ");
+//     let mut name = String::new();
+//     let greeting = "Nice to meet you";
+//     io::stdin().read_line(&mut name)
+//         .expect("Dind't Receive Input!");
+//     println!("Hello {}!, {}", name.trim_end(), greeting);
+// }
+
+fn main() {
+    const ONE_MIL: u32 = 1_000_000;
+    const PI: f32 = 3.14159;
+    let age = "25";
+    let mut age: u32 = age.trim().parse()
+        .expect("Age wasn't assigned a number");
+    age += 1;
+    println!("I'm {} and I want ${}", age, ONE_MIL);
+
 }
