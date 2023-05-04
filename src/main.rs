1057f056bc1e59badbbcfbc7df5d26fad54fd18c
Amir Zand <am.zand@outlook.com>
2023-05-05 01:07:11 +0430

    simple input

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,15 @@ use std::io::{Write, BufReader, BufRead, ErrorKind};
 use std::fs::File;
 use std::cmp::Ordering;
 
-fn main() {
-    println!("Hello World!");
+// fn main() {
+//     println!("Hello World!");
+// }
+
+fn main () {
+    println!("Enter your name: ");
+    let mut name = String::new();
+    let greeting = "Nice to meet you";
+    io::stdin().read_line(&mut name)
+        .expect("Dind't Receive Input!");
+    println!("Hello {}!, {}", name.trim_end(), greeting);
 }
